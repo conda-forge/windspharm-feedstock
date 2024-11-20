97fbe0166862003a43371b2118fc2b2e4268ff3e
Andrew Dawson <ajdatm@gmail.com>
2024-11-20 17:48:18 +0000

    Simplify tests

--- recipe/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run windspharm test suite in its own directory.
-
-mkdir windspharm-tests && cd windspharm-tests || exit 1
-pytest -vrsx --pyargs windspharm
-stat=$?
-cd .. && rm -rf windspharm-tests || exit 2
-exit $stat
